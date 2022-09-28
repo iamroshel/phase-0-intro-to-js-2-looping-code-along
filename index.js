b31c0e97df9ed87bd56a12a03d3ee94bad0a01fa
Roshel Esteron <mynameisroshel19@gmail.com>
2022-09-28 22:28:56 +1000

    Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+const cardNames = ["Guadalupe", "Ollie", "Aki"]; 
+function writeCards(cardNames) {
+    let cards = []; 
+    for ( let i=0; i<cardNames.length; i++) {
+        cards.push(`Thank you, ${cardNames[i]}, for the wonderful surprise gift!`);
+    }
+    return cards; 
+}
+function countDown() {
+    let count = 11;
+    while (count--) {
+        console.log(count); 
+    }    
+}
